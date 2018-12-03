0ee8da105e08745b37d7eb6ed5d69ca124d443d1
Andrey Sobol <andrey.sobol@bpmobile.com>
2018-12-03 17:14:29 +0300

    Fixed: 1. Specify the correct email. 2. Correct font-size for email-box for mobile devices. 3. Specify prohibited folders for indexing robots.

--- gulpfile.js
+++ gulpfile.js
@@ -88,7 +88,7 @@ gulp.task('extras', () => {
     '!app/scss/**/*.*',
     '!app/components/**/*.*',
     '!app/js/**/*.*',
-    '!app/img/**/*.*',
+    'app/img/**/*.*',
     'app/*.php',
     'app/*.html',
     'app/*.**'
